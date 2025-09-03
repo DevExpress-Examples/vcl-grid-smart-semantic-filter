a9b1be81cb91ec3a9b46605d7fd23560a5b2e883
corp\Sergey.Sammal <Sergey.Sammal@devexpress.com>
2025-09-03 16:25:12 +0300

    Project search paths

--- SimilaritySearch.dpr
+++ SimilaritySearch.dpr
@@ -3,7 +3,6 @@ program SimilaritySearch;
 uses
   Vcl.Forms,
   Similarity in 'Similarity.pas' {frmSimilarity},
-  BertTokenizer in 'C:\Sources_GIT\Misc\BertTokenizer4D\Src\BertTokenizer\BertTokenizer.pas',
   SimilaritySearch.dxSettings in 'SimilaritySearch.dxSettings.pas';
 
 {$R *.res}
